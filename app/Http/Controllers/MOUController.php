7c61c1cead26a392dbbde549aad0331b94a497d9
bamboozaa <chukadee@gmail.com>
2024-04-22 13:44:58 +0700

    PC Commit

--- app/Http/Controllers/MOUController.php
+++ app/Http/Controllers/MOUController.php
@@ -35,11 +35,11 @@ class MOUController extends Controller
         } else if (is_null($request->input('dep_id')) && !is_null($request->input('start_date')) && is_null($request->input('end_date'))) {
             $MOUs = MOU::where('start_date', '>=', $request->input('start_date'))->get();
         } else if (is_null($request->input('dep_id')) && is_null($request->input('start_date')) && !is_null($request->input('end_date'))) {
-            $MOUs = MOU::where('end_date', '>=', $request->input('end_date'))->get();
+            $MOUs = MOU::where('end_date', '<=', $request->input('end_date'))->get();
         } else if (is_null($request->input('dep_id')) && !is_null($request->input('start_date')) && !is_null($request->input('end_date'))) {
             $MOUs = MOU::where('start_date', '>=', $request->input('start_date'))->where('end_date', '<=', $request->input('end_date'))->get();
         } else if (!is_null($request->input('dep_id')) && is_null($request->input('start_date')) && !is_null($request->input('end_date'))) {
-            $MOUs = MOU::where('dep_id', $request->input('dep_id'))->where('end_date', '>=', $request->input('end_date'))->get();
+            $MOUs = MOU::where('dep_id', $request->input('dep_id'))->where('end_date', '<=', $request->input('end_date'))->get();
         }
 
         return view('mou.index', compact('MOUs', 'departments'));
