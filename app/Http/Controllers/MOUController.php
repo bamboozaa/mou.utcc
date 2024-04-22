1aae9561c9e3fcd2c7921dedb88dde08b0d02ce7
bamboozaa <chukadee@gmail.com>
2024-04-22 16:43:41 +0700

    PC Commit

--- app/Http/Controllers/MOUController.php
+++ app/Http/Controllers/MOUController.php
@@ -25,21 +25,21 @@ class MOUController extends Controller
         // $MOUs = MOU::all();
         $departments = Department::pluck('dep_name', 'dep_id');
         if (is_null($request->input('dep_id')) && is_null($request->input('start_date')) && is_null($request->input('end_date'))) {
-            $MOUs = MOU::all();
+            $MOUs = MOU::latest()->get();
         } else if (!is_null($request->input('dep_id')) && is_null($request->input('start_date')) && is_null($request->input('end_date'))) {
-            $MOUs = MOU::where('dep_id', $request->input('dep_id'))->get();
+            $MOUs = MOU::where('dep_id', $request->input('dep_id'))->latest()->get();
         } else if (!is_null($request->input('dep_id')) && !is_null($request->input('start_date')) && is_null($request->input('end_date'))) {
-            $MOUs = MOU::where('dep_id', $request->input('dep_id'))->where('start_date', '>=', $request->input('start_date'))->get();
+            $MOUs = MOU::where('dep_id', $request->input('dep_id'))->where('start_date', '>=', $request->input('start_date'))->latest()->get();
         } else if (!is_null($request->input('dep_id')) && !is_null($request->input('start_date')) && !is_null($request->input('end_date'))) {
-            $MOUs = MOU::where('dep_id', $request->input('dep_id'))->where('start_date', '>=', $request->input('start_date'))->where('end_date', '<=', $request->input('end_date'))->get();
+            $MOUs = MOU::where('dep_id', $request->input('dep_id'))->where('start_date', '>=', $request->input('start_date'))->where('end_date', '<=', $request->input('end_date'))->latest()->get();
         } else if (is_null($request->input('dep_id')) && !is_null($request->input('start_date')) && is_null($request->input('end_date'))) {
-            $MOUs = MOU::where('start_date', '>=', $request->input('start_date'))->get();
+            $MOUs = MOU::where('start_date', '>=', $request->input('start_date'))->latest()->get();
         } else if (is_null($request->input('dep_id')) && is_null($request->input('start_date')) && !is_null($request->input('end_date'))) {
-            $MOUs = MOU::where('end_date', '<=', $request->input('end_date'))->get();
+            $MOUs = MOU::where('end_date', '<=', $request->input('end_date'))->latest()->get();
         } else if (is_null($request->input('dep_id')) && !is_null($request->input('start_date')) && !is_null($request->input('end_date'))) {
-            $MOUs = MOU::where('start_date', '>=', $request->input('start_date'))->where('end_date', '<=', $request->input('end_date'))->get();
+            $MOUs = MOU::where('start_date', '>=', $request->input('start_date'))->where('end_date', '<=', $request->input('end_date'))->latest()->get();
         } else if (!is_null($request->input('dep_id')) && is_null($request->input('start_date')) && !is_null($request->input('end_date'))) {
-            $MOUs = MOU::where('dep_id', $request->input('dep_id'))->where('end_date', '<=', $request->input('end_date'))->get();
+            $MOUs = MOU::where('dep_id', $request->input('dep_id'))->where('end_date', '<=', $request->input('end_date'))->latest()->get();
         }
 
         return view('mou.index', compact('MOUs', 'departments'));
